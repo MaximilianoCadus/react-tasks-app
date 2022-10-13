30bf1e93109761d87df8888443c9717b74d0d3c4
Maxi Cadús <maxicadus172@gmail.com>
2022-10-13 14:41:21 -0300

    Finished to deploy

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "First task",
+    description: "This is my first task",
+  },
+  {
+    id: 1,
+    title: "Second task",
+    description: "This is my second task",
+  },
+  {
+    id: 2,
+    title: "Third task",
+    description: "This is my third task",
+  },
+];
